68d4a38c6b2435af8258c96dfc1470428c60a50f
Drew <ingels.da@gmail.com>
2016-03-31 18:21:48 -0600

    Code 6:30 Tursday

--- RobotCodeRestart/src/org/usfirst/frc/team4627/robot/commands/SystemTest.java
+++ RobotCodeRestart/src/org/usfirst/frc/team4627/robot/commands/SystemTest.java
@@ -20,16 +20,16 @@ public class SystemTest extends CommandGroup {
     	addSequential(new ShiftLow());
     	System.out.println("Reseting Encoder");
     	addSequential(new ResetChupasEncoder());
-    	System.out.println("Turning to 30 degrees");
+    	System.out.println("Turning to right 30 degrees");
     	addSequential(new AutoTurn(30));
-    	System.out.println("Turning to -60 degrees");
+    	System.out.println("Turning to left 60 degrees");
     	addSequential(new AutoTurn(-60));
-    	System.out.println("Turning back to 30 degrees");
+    	System.out.println("Turning back");
     	addSequential(new AutoTurn(30));
-    	System.out.println("Spit that out");
+    	System.out.println("Spinning el chupas out");
     	addSequential(new SetShootSpeed(1));
     	addSequential(new Wait(1));
-    	System.out.println("Suck that in");
+    	System.out.println("Spinning el chupas in");
     	addSequential(new SetShootSpeed(-1));
     	addSequential(new Wait(1));
     	System.out.println("Stop chupas");
@@ -39,15 +39,13 @@ public class SystemTest extends CommandGroup {
     	addSequential(new Wait(1));
     	System.out.println("Lowering arms fully");
     	addSequential(new SetArmPosition(0));
-    	System.out.println("Starting ");
+    	System.out.println("Starting kicker ");
     	addSequential(new ElChupaKick());
     	System.out.println("Shifting into high gear");
     	addSequential(new ShiftHigh());
     	System.out.println("Shifting to low gear");
     	addSequential(new ShiftLow());
-    	System.out.println("All completed successfully! DREW SMASH");
-
-    	
+    	System.out.println("All completed successfully!");
     	Date s = new Date();
     	String end = df.format(s);
     	System.out.println("End Time: " + end);
